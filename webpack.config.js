54b5947fcf1e81f75678f12066b246a0c54d8c44
Justine Bateman <justinebateman@outlook.com>
2021-04-29 14:08:19 +0100

    Updates to latest versions of dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: {
-        "swagger-ui-json-tree-plugin" : "./src/index.js"
-    },
-    module: {
-        rules: [{
-			test: /\.js?$/,
-			oneOf: [{            
-				exclude: /node_modules/,
-				loader: 'babel-loader'       
-			}]
-		}],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-	optimization: {
-		minimizer: [new UglifyJsPlugin({
-              cache: true,
-              parallel: true,
-              sourceMap: true
-            })],
-	},	
-	devtool: 'source-map',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: '[name].js',
-        library: 'jsonTreePlugin',
-    },
-    devServer: {
-        contentBase: './demo',
-    }
+  entry: {
+    "swagger-ui-json-tree-plugin": "./src/index.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        oneOf: [
+          {
+            exclude: /node_modules/,
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
+    ],
+  },
+  devtool: "source-map",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "[name].js",
+    library: "jsonTreePlugin",
+  },
+  devServer: {
+    contentBase: "./demo",
+  },
 };
